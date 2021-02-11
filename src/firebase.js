8e774301d8fe3f3de9e309bb3b98d89194d450c6
951237 <951237@gmail.com>
2021-02-12 02:44:30 +0900

    Doc : Update - 기초작업 - 초기화중 에러 발생

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDpCjrZRyOH2M_CFFE0X9wV71IUzWR8jjc",
+    authDomain: "nwitter-efca8.firebaseapp.com",
+    projectId: "nwitter-efca8",
+    storageBucket: "nwitter-efca8.appspot.com",
+    messagingSenderId: "931282315054",
+    appId: "1:931282315054:web:c7a78db150904b65453cbf"
+};
+
+export default firebase.initializeApp(firebaseConfig);
